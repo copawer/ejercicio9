38353a53c53ff86ad7b5338266313c4c22078bb6
Sara Gabriela Díaz Oporto <sara.diaz.oporto@gmail.com>
2017-01-09 11:25:32 -0500

    ejercicio9

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # ejercicio9
+
+Inicio:parrotTrouble
+
+## Introduccion de datos
+
+<h1>Habla</h1>
+<h1>Hora</h1>
+<h1>Mostrar el resultado</h1>
+<h1>Hora</h1>
+
+## proceso
+
+<h1>pedir hora</h1>
+<h1>si el loro esta hablando antes de las 7 hrs o despues de las 20 hrs estamos en problemas</h1>
+<h1>entonces retorna a 1</h1>
+
+<h1>sino retorna a 0</h1>
+
+<h1>mostrar resultado</h1>
+
+fin
---
+++ main.js
@@ -0,0 +1,25 @@
+
+function evaluar(){
+    var habla = document.getElementById("habla").value;
+    var hora = parseFloat(document.getElementById("hora").value);
+    
+    var salida = document.getElementById("salida");
+    var mensaje =document.getElementById("mensaje");
+     
+    if (habla == true && hora < 7 || hora > 20)
+    {
+        //problemas
+        salida.innerHTML ="1"
+    }
+    else
+    {
+        //noproblemas
+        salida.innerHTML ="0"
+    }
+    if (habla == true && hora < 7 || hora > 20){
+        mensaje.innerHTML ="problemas"
+    }
+     else{
+        mensaje.innerHTML ="No problemas"
+    }
+}
